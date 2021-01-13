6ca3e40643977fc245ad527f7f954b947e3e697b
Eyeruz <3yerus@gmail.com>
2021-01-13 16:50:08 -0500

    Done.

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
 class Circle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(prevProps) {
+    if (prevProps === this.props) {
+      return false;
+    }
+  }
 
   render() {
     const { color } = this.props;
     console.log(`A ${color} circle was rendered!`);
-    
-    return (
-      <div className={`circle ${color}`}></div>
-    )
+
+    return <div className={`circle ${color}`}></div>;
   }
 }
 
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 
 class Rating extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       increasing: false,
-      decreasing: false
+      decreasing: false,
     };
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      increasing: this.props.rating < nextProps.rating,
+      decreasing: this.props.rating > nextProps.rating,
+    });
   }
 
   render() {
-    let trend = 'stable';
+    let trend = "stable";
     if (this.state.increasing) {
-      trend = 'increasing';
+      trend = "increasing";
     } else if (this.state.decreasing) {
-      trend = 'decreasing';
+      trend = "decreasing";
     }
 
     return (
@@ -24,7 +29,7 @@ class Rating extends React.Component {
         <p>Rating: {this.props.rating}</p>
         <p>Trend: {trend}</p>
       </div>
-    )
+    );
   }
 }
 
